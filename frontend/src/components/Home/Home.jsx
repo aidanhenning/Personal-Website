7a33ba50423a255468ab84942ad4833421fca4ea
aidanhenning <henningaidan@gmail.com>
2025-07-11 12:09:42 -0700

    finished layout of skills, projects, and cta section of homepage

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,5 +1,17 @@
 import styles from "./Home.module.css";
 import heroImage from "../../assets/personal-headshot.jpg";
+import htmlLogo from "../../assets/icons8-html-5.svg";
+import cssLogo from "../../assets/icons8-css3.svg";
+import javascriptLogo from "../../assets/icons8-javascript.svg";
+import reactLogo from "../../assets/icons8-react.svg";
+import nodejsLogo from "../../assets/icons8-node-js.svg";
+import expressLogo from "../../assets/icons8-express-js.svg";
+import postgresqlLogo from "../../assets/icons8-postgresql.svg";
+import vscodeLogo from "../../assets/icons8-visual-studio-code-2019.svg";
+import gitLogo from "../../assets/icons8-git.svg";
+import githubLogo from "../../assets/icons8-github.svg";
+import projectTemp from "../../assets/project-temp.jpg";
+import aiProfile from "../../assets/ai-profile.jpg";
 
 function Home() {
   return (
@@ -16,31 +28,156 @@ function Home() {
             <button className="btn btn-secondary">Contact Me</button>
           </div>
         </div>
-        <div className={styles.heroImage}>
-          <img src={heroImage} alt="headshot of Aidan" width="200px" />
-        </div>
-      </section>
-      <section className={styles.skills}>
-        <h2>Skills</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JAVASCRIPT</li>
-          <li>REACT</li>
-          <li>NODE JS</li>
-          <li>EXPRESS JS</li>
-          <li>POSTGRESQL</li>
-          <li>GIT</li>
-        </ul>
-      </section>
-      <section className={styles.projects}>
-        <h2>Projects</h2>
         <div>
-          <div>Personal Website</div>
-          <div>Property Management App</div>
-          <div>Artist Storefront</div>
+          <img
+            className={styles.heroImage}
+            src={aiProfile}
+            alt="headshot of Aidan"
+          />
         </div>
       </section>
+
+      <hr />
+
+      <section className={styles.skills}>
+        <h2 className={styles.skillsHeader}>My Stack</h2>
+        <div className={styles.skillsCard}>
+          <h3>Frontend</h3>
+          <ul>
+            <li>
+              <img src={htmlLogo} alt="html logo" />
+              <p>HTML</p>
+            </li>
+            <li>
+              <img src={cssLogo} alt="css logo" />
+              <p>CSS</p>
+            </li>
+            <li>
+              <img src={javascriptLogo} alt="javascript logo" />
+              <p>Javascript</p>
+            </li>
+            <li>
+              <img src={reactLogo} alt="react logo" />
+              <p>React</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.skillsCard}>
+          <h3>Backend</h3>
+          <ul>
+            <li>
+              <img src={nodejsLogo} alt="node js logo" />
+              <p>Node JS</p>
+            </li>
+            <li>
+              <img src={expressLogo} alt="express js logo" />
+              <p>Express JS</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.skillsCard}>
+          <h3>Database</h3>
+          <ul>
+            <li>
+              <img src={postgresqlLogo} alt="postgresql logo" />
+              <p>Postgresql</p>
+            </li>
+          </ul>
+        </div>
+        <div className={styles.skillsCard}>
+          <h3>Tools</h3>
+          <ul>
+            <li>
+              <img src={vscodeLogo} alt="vscode logo" />
+              <p>VSCode</p>
+            </li>
+            <li>
+              <img src={gitLogo} alt="git logo" />
+              <p>GIT</p>
+            </li>
+            <li>
+              <img src={githubLogo} alt="github logo" />
+              <p>Github</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <hr />
+
+      <section className={styles.projects}>
+        <h2>My Work</h2>
+        <div className={styles.projectCards}>
+          <div class={styles.projectCard}>
+            <div class={styles.projectImageContainer}>
+              <img src={projectTemp} alt="website snapshot" />
+            </div>
+            <div class={styles.projectCardDescription}>
+              <h2>Project Title</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+                pariatur esse rerum? Maiores, nemo! Qui cupiditate, natus
+                consequatur sint labore eos doloribus, minus veritatis nostrum
+                eius placeat. Ratione, mollitia facere!
+              </p>
+              <div class={styles.projectTechButtons}>
+                <p class={styles.projectTechButton}>html</p>
+                <p class={styles.projectTechButton}>css</p>
+                <p class={styles.projectTechButton}>javascript</p>
+                <p class={styles.projectTechButton}>react</p>
+              </div>
+              <button className="btn btn-primary">Explore →</button>
+            </div>
+          </div>
+
+          <div class={styles.projectCard}>
+            <div class={styles.projectImageContainer}>
+              <img src={projectTemp} alt="website snapshot" />
+            </div>
+            <div class={styles.projectCardDescription}>
+              <h2>Project Title</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+                pariatur esse rerum? Maiores, nemo! Qui cupiditate, natus
+                consequatur sint labore eos doloribus, minus veritatis nostrum
+                eius placeat. Ratione, mollitia facere!
+              </p>
+              <div class={styles.projectTechButtons}>
+                <p class={styles.projectTechButton}>html</p>
+                <p class={styles.projectTechButton}>css</p>
+                <p class={styles.projectTechButton}>javascript</p>
+                <p class={styles.projectTechButton}>react</p>
+              </div>
+              <button className="btn btn-primary">Explore →</button>
+            </div>
+          </div>
+
+          <div class={styles.projectCard}>
+            <div class={styles.projectImageContainer}>
+              <img src={projectTemp} alt="website snapshot" />
+            </div>
+            <div class={styles.projectCardDescription}>
+              <h2>Project Title</h2>
+              <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+                pariatur esse rerum? Maiores, nemo! Qui cupiditate, natus
+                consequatur sint labore eos doloribus, minus veritatis nostrum
+                eius placeat. Ratione, mollitia facere!
+              </p>
+              <div class={styles.projectTechButtons}>
+                <p class={styles.projectTechButton}>html</p>
+                <p class={styles.projectTechButton}>css</p>
+                <p class={styles.projectTechButton}>javascript</p>
+                <p class={styles.projectTechButton}>react</p>
+              </div>
+              <button className="btn btn-primary">Explore →</button>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <hr />
+
       <section className={styles.cta}>
         <h2>Hire me!</h2>
         <p>
