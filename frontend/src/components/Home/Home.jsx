1a3c9174fe612c3a88d2cdc45f659c1cbf6ea2fa
aidanhenning <henningaidan@gmail.com>
2025-07-28 15:01:53 -0700

    changed the my stack section to be more compact and readable using display grid

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -11,6 +11,10 @@ import gitLogo from "../../assets/icons8-git.svg";
 import githubLogo from "../../assets/icons8-github.svg";
 import projectTemp from "../../assets/project-temp.jpg";
 import aiProfile from "../../assets/ai-profile.jpg";
+import frontendIcon from "../../assets/icons8-code-100.png";
+import backendIcon from "../../assets/icons8-server-100.png";
+import databaseIcon from "../../assets/icons8-database-100.png";
+import toolsIcon from "../../assets/icons8-tools-96.png";
 
 function Home() {
   return (
@@ -42,65 +46,79 @@ function Home() {
 
       <section className={styles.skills}>
         <h2 className={styles.skillsHeader}>My Stack</h2>
-        <div className={styles.skillsCard}>
-          <h3>Frontend</h3>
-          <ul>
-            <li>
-              <img src={htmlLogo} alt="html logo" />
-              <p>HTML</p>
-            </li>
-            <li>
-              <img src={cssLogo} alt="css logo" />
-              <p>CSS</p>
-            </li>
-            <li>
-              <img src={javascriptLogo} alt="javascript logo" />
-              <p>Javascript</p>
-            </li>
-            <li>
-              <img src={reactLogo} alt="react logo" />
-              <p>React</p>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.skillsCard}>
-          <h3>Backend</h3>
-          <ul>
-            <li>
-              <img src={nodejsLogo} alt="node js logo" />
-              <p>Node JS</p>
-            </li>
-            <li>
-              <img src={expressLogo} alt="express js logo" />
-              <p>Express JS</p>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.skillsCard}>
-          <h3>Database</h3>
-          <ul>
-            <li>
-              <img src={postgresqlLogo} alt="postgresql logo" />
-              <p>Postgresql</p>
-            </li>
-          </ul>
-        </div>
-        <div className={styles.skillsCard}>
-          <h3>Tools</h3>
-          <ul>
-            <li>
-              <img src={vscodeLogo} alt="vscode logo" />
-              <p>VSCode</p>
-            </li>
-            <li>
-              <img src={gitLogo} alt="git logo" />
-              <p>GIT</p>
-            </li>
-            <li>
-              <img src={githubLogo} alt="github logo" />
-              <p>Github</p>
-            </li>
-          </ul>
+        <div className={styles.skillsCards}>
+          <div className={styles.skillsCard}>
+            <div className={styles.skillsCardHeader}>
+              <img src={frontendIcon} alt="frontend icon" />
+              <h3>Frontend</h3>
+            </div>
+            <ul>
+              <li>
+                <img src={htmlLogo} alt="html logo" />
+                <p>HTML</p>
+              </li>
+              <li>
+                <img src={cssLogo} alt="css logo" />
+                <p>CSS</p>
+              </li>
+              <li>
+                <img src={javascriptLogo} alt="javascript logo" />
+                <p>Javascript</p>
+              </li>
+              <li>
+                <img src={reactLogo} alt="react logo" />
+                <p>React</p>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.skillsCard}>
+            <div className={styles.skillsCardHeader}>
+              <img src={backendIcon} alt="backend icon" />
+              <h3>Backend</h3>
+            </div>
+            <ul>
+              <li>
+                <img src={nodejsLogo} alt="node js logo" />
+                <p>Node JS</p>
+              </li>
+              <li>
+                <img src={expressLogo} alt="express js logo" />
+                <p>Express JS</p>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.skillsCard}>
+            <div className={styles.skillsCardHeader}>
+              <img src={databaseIcon} alt="database icon" />
+              <h3>Database</h3>
+            </div>
+            <ul>
+              <li>
+                <img src={postgresqlLogo} alt="postgresql logo" />
+                <p>Postgresql</p>
+              </li>
+            </ul>
+          </div>
+          <div className={styles.skillsCard}>
+            <div className={styles.skillsCardHeader}>
+              <img src={toolsIcon} alt="tools icon" />
+              <h3>Tools</h3>
+            </div>
+            <ul>
+              <li>
+                <img src={vscodeLogo} alt="vscode logo" />
+                <p>VSCode</p>
+              </li>
+              <li>
+                <img src={gitLogo} alt="git logo" />
+                <p>GIT</p>
+              </li>
+              <li>
+                <img src={githubLogo} alt="github logo" />
+                <p>Github</p>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
 
