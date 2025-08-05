ef7d0f9484eed57860ce03fc3ee6c5b03421001a
aidanhenning <henningaidan@gmail.com>
2025-08-05 09:31:21 -0700

    styled and filled in about me section with information from LinkedIn

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -3,22 +3,26 @@ import React from "react";
 export default function About() {
   return (
     <div id="about">
-      <h2>About</h2>
-      <figure>
-        <img
-          src="https://www.vhv.rs/dpng/d/191-1918102_circle-profile-chengsu-chen-hd-png-download.png"
-          alt="Profile Picture"
-        />
-        <figcaption>Me!</figcaption>
-      </figure>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        saepe repellat, dolores aspernatur porro est expedita quae delectus id
-        qui natus! Asperiores, impedit? Cupiditate, temporibus provident
-        veritatis ut error omnis! Nobis, suscipit non sunt eveniet hic officia.
-        Illum perspiciatis reiciendis deserunt quam magnam quos obcaecati illo
-        nobis, molestiae eveniet cum quidem! Sint temporibus obcaecati corrupti
-        vitae fuga perferendis quaerat corporis?
+      <h2 className="text-center text-black dark:text-white text-4xl md:text-5xl font-bold mb-6">
+        About Me
+      </h2>
+      <p className="text-center text-black dark:text-white mb-2">
+        Hi, I’m Aidan Henning - a Software Developer and recent graduate of
+        Fullstack Academy with a background in real estate management.
+      </p>
+      <p className="text-center text-black dark:text-white mb-2">
+        While managing multifamily properties, I discovered my interest in
+        coding. What started as a personal curiosity quickly grew into a genuine
+        passion for building projects and solving complex problems. As a result,
+        I decided to make a career pivot and enroll in a software development
+        bootcamp.
+      </p>
+      <p className="text-center text-black dark:text-white mb-8">
+        Today, I enjoy applying my problem-solving skills, creativity, and
+        attention to detail to build applications that create real value for
+        people. I’m excited to grow as a fullstack developer, learn from
+        experienced teams, and contribute to building meaningful web
+        applications that improve people’s lives.
       </p>
     </div>
   );
