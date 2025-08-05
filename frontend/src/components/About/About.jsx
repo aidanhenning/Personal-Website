ecbab7d89b41c906400c8e5f12e8e31e654dc608
aidanhenning <henningaidan@gmail.com>
2025-08-05 09:33:02 -0700

    upped the about me font size to lg

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -6,18 +6,18 @@ export default function About() {
       <h2 className="text-center text-black dark:text-white text-4xl md:text-5xl font-bold mb-6">
         About Me
       </h2>
-      <p className="text-center text-black dark:text-white mb-2">
+      <p className="text-center text-lg text-black dark:text-white mb-2">
         Hi, I’m Aidan Henning - a Software Developer and recent graduate of
         Fullstack Academy with a background in real estate management.
       </p>
-      <p className="text-center text-black dark:text-white mb-2">
+      <p className="text-center text-lg text-black dark:text-white mb-2">
         While managing multifamily properties, I discovered my interest in
         coding. What started as a personal curiosity quickly grew into a genuine
         passion for building projects and solving complex problems. As a result,
         I decided to make a career pivot and enroll in a software development
         bootcamp.
       </p>
-      <p className="text-center text-black dark:text-white mb-8">
+      <p className="text-center text-lg text-black dark:text-white mb-8">
         Today, I enjoy applying my problem-solving skills, creativity, and
         attention to detail to build applications that create real value for
         people. I’m excited to grow as a fullstack developer, learn from
