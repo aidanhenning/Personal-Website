6f431e56c73109ca89a1aeb4b6d4d3a5b6fc9098
aidanhenning <henningaidan@gmail.com>
2025-08-05 11:40:59 -0700

    completed footer styling

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,13 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <p>&copy; 2025 Aidan Henning. All Rights Reserved</p>
+      <p className="text-center text-sm text-black dark:text-white">
+        &copy; 2025{" "}
+        <span className="bg-gradient-to-r from-[#a855f7] to-[#6366f1] bg-clip-text text-transparent">
+          Aidan Henning.
+        </span>{" "}
+        All Rights Reserved
+      </p>
     </>
   );
 }
