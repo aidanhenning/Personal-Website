584d410be6064cb846ff80f5b987d89c1e6f706f
aidanhenning <henningaidan@gmail.com>
2025-08-05 09:46:49 -0700

    changed about me section heading color

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function About() {
   return (
     <div id="about" className="mx-auto px-[10%]">
-      <h2 className="text-center text-black dark:text-white text-4xl md:text-5xl font-bold mb-6">
+      <h2 className="text-center text-4xl md:text-5xl font-bold bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text text-transparent mb-6">
         About Me
       </h2>
       <p className="text-center text-lg text-black dark:text-white mb-2">
