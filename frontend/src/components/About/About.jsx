69afe568b13f5dba7e5547160ae0c80cd76d324f
aidanhenning <henningaidan@gmail.com>
2025-08-06 12:52:43 -0700

    added fade in animation to about me section

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -3,21 +3,21 @@ import React from "react";
 export default function About() {
   return (
     <div id="about" className="mx-auto px-[10%] mb-[10%]">
-      <h2 className="text-center text-4xl md:text-5xl font-bold bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text text-transparent mb-6">
+      <h2 className="text-center text-4xl md:text-5xl font-bold bg-gradient-to-r from-[#6366f1] to-[#a855f7] bg-clip-text text-transparent mb-6 animate-appear">
         About Me
       </h2>
-      <p className="text-center text-lg text-black dark:text-white mb-2">
+      <p className="text-center text-lg text-black dark:text-white mb-2 animate-appear">
         Hi, I’m Aidan Henning - a Software Developer and recent graduate of
         Fullstack Academy with a background in real estate management.
       </p>
-      <p className="text-center text-lg text-black dark:text-white mb-2">
+      <p className="text-center text-lg text-black dark:text-white mb-2 animate-appear">
         While managing multifamily properties, I discovered my interest in
         coding. What started as a personal curiosity quickly grew into a genuine
         passion for building projects and solving complex problems. As a result,
         I decided to make a career pivot and enroll in a software development
         bootcamp.
       </p>
-      <p className="text-center text-lg text-black dark:text-white mb-2">
+      <p className="text-center text-lg text-black dark:text-white mb-2 animate-appear">
         Today, I enjoy applying my problem-solving skills, creativity, and
         attention to detail to build applications that create real value for
         people. I’m excited to grow as a fullstack developer, learn from
